38e3af5b30656716a77c18a7685dc9a71340d26c
Michael Gillon <Michael@blocksui.com>
2021-01-29 23:32:11 +0000

    add thrusters

--- Assets/Scripts/ShipController.cs
+++ Assets/Scripts/ShipController.cs
@@ -9,6 +9,7 @@ public class ShipController : MonoBehaviour
 
     private Rigidbody _rigidbody;
 
+    private Thruster[] _allThrusters;
     private Thruster[] _leftThrusters;
     private Thruster[] _rearThrusters;
     private Thruster[] _rightThrusters;
@@ -17,40 +18,47 @@ public class ShipController : MonoBehaviour
     void Start()
     {
         _rigidbody = GetComponent<Rigidbody>();
-        var allThrusters = GetComponentsInChildren<Thruster>();
+        _allThrusters = GetComponentsInChildren<Thruster>();
 
-        _rightThrusters = allThrusters.Where(w => w.transform.eulerAngles.y == 90).ToArray();
-        _leftThrusters = allThrusters.Where(w => w.transform.eulerAngles.y == 270).ToArray();
-        _rearThrusters = allThrusters.Where(w => w.transform.eulerAngles.y == 0).ToArray();
+        var angles = _allThrusters.Select(w => w.transform.eulerAngles).ToArray();
+
+        _rightThrusters = _allThrusters.Where(w => w.transform.eulerAngles.y == 90).ToArray();
+        _leftThrusters = _allThrusters.Where(w => w.transform.eulerAngles.y == 270).ToArray();
+        _rearThrusters = _allThrusters.Where(w => w.transform.eulerAngles.y == 0).ToArray();
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey(KeyCode.W))
+        foreach (var thruster in _allThrusters)
         {
-            foreach (var thruster in _rearThrusters)
-            {
-                _rigidbody.AddForceAtPosition(thruster.ThrustForce * thruster.transform.forward * Time.deltaTime, thruster.transform.position);
-            }
+            thruster.LightEffect.enabled = false;
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            FireThruster(_rearThrusters);
         }
 
         if (Input.GetKey(KeyCode.A))
         {
-            foreach (var thruster in _leftThrusters)
-            {
-                _rigidbody.AddForceAtPosition(thruster.ThrustForce * thruster.transform.forward * Time.deltaTime, thruster.transform.position);
-            }
+            FireThruster(_leftThrusters);
         }
 
         if (Input.GetKey(KeyCode.D))
         {
-            foreach (var thruster in _rightThrusters)
-            {
-                _rigidbody.AddForceAtPosition(thruster.ThrustForce * thruster.transform.forward * Time.deltaTime, thruster.transform.position);
-            }
+            FireThruster(_rightThrusters);
         }
 
+    }
+
+    private void FireThruster(Thruster[] thrusters)
+    {
+        foreach (var thruster in thrusters)
+        {
+            _rigidbody.AddForceAtPosition(thruster.ThrustForce * (thruster.transform.forward) * Time.deltaTime, thruster.transform.position);
+            thruster.LightEffect.enabled = true;
+        }
     }
 }
